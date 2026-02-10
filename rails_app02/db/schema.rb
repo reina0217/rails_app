d47dbefad2735054df245c8195f8a48c871d6997
Reina <rein0217a@icloud.com>
2026-02-10 21:02:32 +0900

    chore: セットアップに必要なファイルを更新

--- rails_app02/db/schema.rb
+++ rails_app02/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_27_074423) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_10_120046) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
