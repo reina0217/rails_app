9b1f4626f9e935e14c15e33ff1dcaeafb226927d
Reina <rein0217a@icloud.com>
2026-03-09 22:43:56 +0900

    chore: #1 画像アップロードのフォームを追加

--- creating-blog-new-master/app/models/article.rb
+++ creating-blog-new-master/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :text,  presence: true, length: { maximum: 10000 }
+  mount_uploader :image, ImageUploader
 end
